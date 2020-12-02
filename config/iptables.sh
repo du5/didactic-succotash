cb11714bc2309e679123a9842e382f589a7e9560
干净 <54979375+du5@users.noreply.github.com>
2020-12-03 02:05:52 +0800

    Create iptables.sh

---
+++ config/iptables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+systemctl stop firewalld.service && systemctl disable firewalld.service && systemctl mask firewalld.service
+
+yum install initscripts iptables iptables-services -y
+
+systemctl enable iptables.service --now
+
+iptables -F && iptables -X && iptables -Z && iptables -t nat -F && iptables -P INPUT ACCEPT && iptables -P FORWARD ACCEPT && iptables -P OUTPUT ACCEPT
+
+service iptables save
