cc6395beb33eac6f1e47a2856ab7f17b1a8c61cb
干净 <54979375+du5@users.noreply.github.com>
2020-12-03 02:53:32 +0800

    Create bbr.sh

---
+++ config/bbr.sh
@@ -0,0 +1,45 @@
+yum -y install bc
+
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+
+yum --enablerepo=elrepo-kernel install kernel-ml -y
+
+egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \'
+
+echo "———————————————————————————————————"
+
+read -p "请输入序号选择内核(第一项序号为 0, 默认选择 0 ):" kernel_id
+
+echo "———————————————————————————————————"
+
+sed -i '/soft nofile/d' /etc/security/limits.conf
+
+sed -i '/hard nofile/d' /etc/security/limits.conf
+
+echo "* soft nofile 51200" >> /etc/security/limits.conf
+
+echo "* hard nofile 51200" >> /etc/security/limits.conf
+
+echo "root soft nofile 51200" >> /etc/security/limits.conf
+
+echo "root hard nofile 51200"  >> /etc/security/limits.conf
+
+cat /etc/security/limits.conf | grep nofile
+
+echo 3 > /proc/sys/net/ipv4/tcp_fastopen
+
+echo "vm.swappiness = 25" >> /etc/sysctl.conf
+
+echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
+
+echo "net.core.default_qdisc = cake" >> /etc/sysctl.conf
+
+echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
+
+echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.conf
+
+read -p "请按任意键重启，如需手动重启使用 Ctrl+C 退出。重启后需要执行 $0 2" var
+
+reboot
