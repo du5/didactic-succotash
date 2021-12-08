a262f4f7f81e9e74a5fa736f88be0700d78f273f
干净 <54979375+du5@users.noreply.github.com>
2021-12-08 20:20:58 +0800

    Update docker.sh

--- install/docker.sh
+++ install/docker.sh
@@ -4,11 +4,11 @@ rm -rf ${0}
 
 apt -y install curl || yum -y install curl libseccomp-devel
 
-curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+curl -fsSL https://get.docker.com | bash 
 
 systemctl enable docker --now
 
-curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+wget "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-$(uname -s)-$(uname -m)" -O /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
