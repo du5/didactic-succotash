3a8817409cb103b4ac9d7e4b59fdfe6f8fc56f51
干净 <54979375+du5@users.noreply.github.com>
2021-12-08 20:22:16 +0800

    Update d-nginx.sh

--- install/d-nginx.sh
+++ install/d-nginx.sh
@@ -2,11 +2,9 @@
 
 rm -rf ${0}
 
-apt install curl gnupg2 ca-certificates lsb-release
+apt install curl gnupg2 ca-certificates lsb-release -y
 
-echo "deb http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
-
+echo "deb http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
 
 curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
 
