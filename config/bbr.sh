894742601d2730b0feb4d9eeb1e2458d18a02cd9
干净 <54979375+du5@users.noreply.github.com>
2021-12-29 14:43:07 +0800

    Create bbr.sh

--- config/bbr.sh
+++ config/bbr.sh
@@ -42,7 +42,7 @@ echo "" > /etc/sysctl.conf
 
 echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
 
-echo "net.core.default_qdisc = cake" >> /etc/sysctl.conf
+echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
 
 echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
 
