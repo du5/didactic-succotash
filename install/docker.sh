c7848487423db068e678dbaca5bb8cb533cf1ebb
干净 <54979375+du5@users.noreply.github.com>
2022-09-16 16:38:39 +0800

    Update docker.sh

--- install/docker.sh
+++ install/docker.sh
@@ -8,7 +8,7 @@ curl -fsSL https://get.docker.com | bash
 
 systemctl enable docker --now
 
-wget "https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-$(uname -s)-$(uname -m)" -O /usr/local/bin/docker-compose
+wget "https://github.com/docker/compose/releases/download/v2.11.0/docker-compose-$(uname -s)-$(uname -m)" -O /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
