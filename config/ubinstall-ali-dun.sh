859f757afeb5d7f44859e3ff3dd47175ff59ec30
干净 <54979375+du5@users.noreply.github.com>
2023-09-17 23:34:44 +0800

    Update ubinstall-ali-dun.sh

--- config/ubinstall-ali-dun.sh
+++ config/ubinstall-ali-dun.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# curl -s https://raw.githubusercontent.com/du5/didactic-succotash/master/config/ubinstall-ali-dun.sh | bash 
+
 rm -rf ${0}
 
 curl -sSL http://update.aegis.aliyun.com/download/quartz_uninstall.sh | bash
 
-rm -rf /usr/local/aegis /usr/sbin/aliyun-service /lib/systemd/system/aliyun.service
+rm -rf /usr/local/aegis 
+
+systemctl disable --now aliyun.service
+
+/usr/local/share/assist-daemon/assist_daemon --stop
+
+/usr/local/share/assist-daemon/assist_daemon --delete
+
+rm -rf /usr/local/share/assist-daemon
